0b7b52a1b48c3e9eedfd1e507ceab240430e548f
Flaroon <68732404+Flaroon@users.noreply.github.com>
2021-08-22 22:06:20 +1000

    Update Circle.cs

--- Mesh Generators/Circle.cs
+++ Mesh Generators/Circle.cs
@@ -14,7 +14,7 @@ public class Circle : MonoBehaviour
             v.Add(new Vector3(x, 0, y));
         }
         for (int l = 0; l < circle; l++){
-            this.t.AddRange(new List<int>() {v.Count, v.Count + 1,v.Count + 2 });
+            t.AddRange(new List<int>() {v.Count, v.Count + 1,v.Count + 2 });
             v.AddRange(new List<Vector3>(){v[l],v[l + 1],Vector3.zero});
         }
         Mesh mesh = new Mesh();
